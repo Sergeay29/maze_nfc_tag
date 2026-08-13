e99c7fcd75cdae61e43d19b6618a8d722c96e66d
Augustecosme <cooviquenum8@gmail.com>
2026-08-13 08:42:58 +0100

    delete(client-pages): remove client-related pages and components
    - Deleted ClientHomePage, ClientProfilePage, and ClientRewardsPage along with their associated styles and logic.
    - Cleaned up index file to reflect the removal of these components, enhancing overall code maintainability.

--- frontend/src/pages/client/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ClientHomePage } from './ClientHomePage';
-export { default as ClientRewardsPage } from './ClientRewardsPage';
-export { default as ClientProfilePage } from './ClientProfilePage';
